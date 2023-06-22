d93f65a8c588a54f6b6e10c12d6ccc5b9c814e77
Stefany Kupicki <stefanygabrielekupicki@gmail.com>
2023-06-22 12:01:08 -0300

    Hard e SoftSkills consumindo da API

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,19 @@ function updateProfileInfo(profileData) {
 
 }
 
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+}
+
 (async () => {
     const profileData = await fetchProfileData();
     updateProfileInfo(profileData);
+    updateSoftSkills(profileData);
+    updateHardSkills(profileData);
 })()

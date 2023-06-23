e71d4b948112ec97c62398e46aa57b04577b937e
Stefany Kupicki <stefanygabrielekupicki@gmail.com>
2023-06-23 12:05:00 -0300

    ProfessionalExp consumindo da API e Atualização de conteúdo

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,9 +34,22 @@ function updateHardSkills(profileData) {
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 
+function updateProfessionalExperience(profileData) {
+    const professionalExperience = document.getElementById('profile.professionalExperience')
+    professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
+        return `
+            <li>
+                <h3 class="title">${experience.name}</h3>
+                <p class="period">${experience.period}</p>
+                <p>${experience.description}</p>
+            </li>
+        `
+    }).join('')
+}
+
 (async () => {
     const profileData = await fetchProfileData();
     updateProfileInfo(profileData);
     updateSoftSkills(profileData);
-    updateHardSkills(profileData);
+    updateProfessionalExperience(profileData);
 })()
